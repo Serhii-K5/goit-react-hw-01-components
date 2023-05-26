e3046870517c22fd7528c3564029f06a12bca845
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-05-26 11:24:30 +0300

    Replaced {children} with detailed description

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,30 @@
-export const App = ({ children }) => {
+// export const App = ({ children }) => {
+//   return (
+//     <div
+//       style={{
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       {children}
+//     </div>
+//   );
+// };
+
+import { Profile } from './profile/Profile';
+import { Statistics } from './statistics/Statistics';
+import { FriendList } from './friend-list/FriendList';
+import { TransactionHistory } from './transaction-history/TransactionHistory';
+import user from './profile/user.json';
+import data from './statistics/data.json';
+import friends from './friend-list/friends.json';
+import transactions from './transaction-history/transactions.json';
+
+export const App = () => {
   return (
     <div
       style={{
@@ -10,7 +36,16 @@ export const App = ({ children }) => {
         color: '#010101',
       }}
     >
-      {children}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />      
     </div>
   );
 };
